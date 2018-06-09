939416bfa8dd52f79584790e0abaf2da16d798dc
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-06-09 20:46:01 +0200

    added map prefab

---
+++ altlas/Assets/scripts/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public Transform map;
+	// Use this for initialization
+	void Start () {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                Instantiate(map, new Vector3(-0.085f, 1, 0), Quaternion.identity);
+            }
+        }
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
