2144f80ef908191850eb43c60dbe4db8f112fc2e
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-06-09 23:48:52 +0200

    globe rotates & made small size adjustmens to scene

---
+++ altlas/Assets/scripts/GlobeRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobeRotation : MonoBehaviour {
+    public GameObject globe;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        globe.transform.Rotate(0, 0, 10 * Time.deltaTime);
+    }
+}
