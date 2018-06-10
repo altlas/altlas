cfeeec73f2e4cf9f2bd8e3fd82649b91082a0487
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-06-10 11:42:30 +0200

    maps are moveable objects

--- altlas/Assets/scripts/ControllerInteraction.cs
+++ altlas/Assets/scripts/ControllerInteraction.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class ControllerInteraction : MonoBehaviour
 {
 
     private SteamVR_TrackedObject trackedObj;
+
+    public GameObject heldObject;
+    public bool isHolding;
 
     private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
@@ -14,7 +16,11 @@ public class ControllerInteraction : MonoBehaviour
     private Valve.VR.EVRButtonId rightPadButton = Valve.VR.EVRButtonId.k_EButton_DPad_Right;
     private Valve.VR.EVRButtonId downPadButton = Valve.VR.EVRButtonId.k_EButton_DPad_Down;
 
-    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_Controller.Device controller {
+        get {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
 
     void Start()
     {
@@ -32,7 +38,37 @@ public class ControllerInteraction : MonoBehaviour
 
         if (controller.GetPressDown(gripButton))
         {
-            Debug.Log("Trigger has been pressed!");
+            if (heldObject != null) {
+                heldObject.transform.parent = this.transform;
+                heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                isHolding = true;
+                Debug.Log("ja");
+            }
         }
+        if (controller.GetPressUp(gripButton))
+        {
+            if (heldObject != null)
+            {
+                heldObject.transform.parent = null;
+                heldObject.GetComponent<Rigidbody>().isKinematic = false;
+                isHolding = false;
+            }
+            
+        }
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.tag.Equals("Pickupable") && !isHolding) {
+            heldObject = collider.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if(!isHolding)
+            heldObject = null;
     }
 }
