c2ef527d7d5000b245428c336bb36ca3fad7db01
Jonas Bounama <jobounama@hotmail.de>
2018-06-11 19:22:32 +0200

    first step

---
+++ altlas/Assets/scripts/DesktopCam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopCam : MonoBehaviour {
+  public float speedH = 2.0f;
+  public float speedV = 2.0f;
+
+  private float yaw = 0.0f;
+  private float pitch = 0.0f;
+  private Camera cam;
+
+  void Start () {
+    Cursor.lockState = CursorLockMode.Locked;
+    cam = GetComponent<Camera>();
+  }
+  
+  void Update () {
+    yaw += speedH * Input.GetAxis("Mouse X");
+    pitch -= speedV * Input.GetAxis("Mouse Y");
+
+    transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit)){
+      if(hit.transform.name.ToString() == "Map_alt(Clone)"){
+        var gameObject = hit.transform.gameObject;
+        gameObject.GetComponent<MapScript>().OnMouseEnter();
+      }
+
+    }
+      //print("I'm looking at nothing!");
+  
+  }
+}
--- altlas/Assets/scripts/MapScript.cs
+++ altlas/Assets/scripts/MapScript.cs
@@ -3,5 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapScript : MonoBehaviour {
-    public MapData data;
+  public MapData data;
+  private Color startcolor;
+  public Renderer rend;
+
+  void start(){
+  }
+
+  public void OnMouseEnter()
+  {
+    rend = GetComponent<MeshRenderer>();
+    startcolor = rend.material.color;
+    rend.material.color = Color.red;
+  }
+
+  public void OnMouseExit()
+  {
+    rend = GetComponent<MeshRenderer>();
+    //rend.material.color = startcolor;
+  }
 }
