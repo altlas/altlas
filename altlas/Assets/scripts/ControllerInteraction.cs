1a1acb101e46bbcd6f524e19ccf7f4140efea60d
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-06-12 18:49:59 +0200

    bugfixes and refactoring

--- altlas/Assets/scripts/ControllerInteraction.cs
+++ altlas/Assets/scripts/ControllerInteraction.cs
@@ -6,11 +6,11 @@ public class ControllerInteraction : MonoBehaviour
 
     private SteamVR_TrackedObject trackedObj;
 
-    private GameObject removedStack = null;
+    /*private GameObject removedStack = null;
     private Vector3 removedStackPosition;
-    MapGenerator instance = new MapGenerator();
+    MapGenerator instance = new MapGenerator();*/
 
-    public GameObject heldObject;
+    public static GameObject heldObject;
     public bool isHolding;
 
     private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
@@ -34,16 +34,14 @@ public class ControllerInteraction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (controller == null)
         {
             Debug.Log("Controller not initialized");
             return;
         }
-
-        gripButtonPressedAction();
-
-        if (removedStack != null) {
-            removedStackPosition = removedStack.transform.position;
+        if (MoveStack.removedStack != null) {
+            MoveStack.removedStackPosition = MoveStack.removedStack.transform.position;
         }
 
 
@@ -61,8 +59,8 @@ public class ControllerInteraction : MonoBehaviour
 
         }
         if (controller.GetPressDown(triggerButton)) {
-            if (removedStack != null) {
-                resetStack();
+            if (MoveStack.removedStack != null) {
+                MoveStack.resetStack();
             }
         }
 
@@ -72,64 +70,26 @@ public class ControllerInteraction : MonoBehaviour
     public void gripButtonPressedAction() {
         if (heldObject != null)
         {
-            if (objectIsFromAStack(heldObject))
+            if (MoveStack.objectIsFromAStack(heldObject))
             {
                 GameObject stack = heldObject.transform.parent.gameObject;
-                if (removedStack != null)
+                if (MoveStack.removedStack != null)
                 {
-                    if (stack.name.Equals(removedStack.transform.name))  //maps on desk are being clicked
+                    if (stack.name.Equals(MoveStack.removedStack.transform.name))  //maps on desk are being clicked
                     {
                         holdObject();
                         return;
-                    } else //another stack was selected while another was on the table, so first move back the maps
-                    {                        
-                        resetStack();
                     }
+                    //another stack was selected while another was on the table, so first move back the maps        
+                    MoveStack.resetStack();
+                    isHolding = false;
                 }
-                moveStackToDesk(stack);
+                MoveStack.moveStackToDesk(stack);
             }
             else {
                 holdObject();
             }
         }
-    }
-
-    
-    public bool objectIsFromAStack(GameObject obj) {
-        if (obj == null) return false;
-        if (obj.transform.parent != null) {
-            if (heldObject.transform.parent.name.Contains(instance.EMPTY_ENDING)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void moveStackToDesk(GameObject stackToMove) {
-        for (int i = 0; i < stackToMove.transform.childCount; i++)
-        {
-            instance.spreadGameObjectOnDesk(stackToMove.transform.GetChild(i).gameObject);
-        }
-        removedStack = stackToMove;
-    }
-
-    public void resetStack() {
-        string nameOfMapsObjects = removedStack.transform.name.Remove(removedStack.transform.name.Length - instance.EMPTY_ENDING.Length);
-        int i = 0;
-        foreach (GameObject map in GameObject.FindGameObjectsWithTag("Pickupable"))
-            {
-                if (map.name.Equals(nameOfMapsObjects))
-                {
-                    map.transform.position = new Vector3(removedStackPosition.x, removedStackPosition.y + i* instance.MAPS_Y_OFFSET, removedStackPosition.z);
-                    map.transform.localScale = new Vector3(0.1f, map.transform.localScale.y, 0.1f);
-                    map.transform.localRotation = Quaternion.identity;
-                    if (map.transform.parent == null) {
-                    map.transform.parent = removedStack.transform;
-                    }
-                }
-             i++;
-         }
-        removedStack = null;
     }
 
     private void holdObject() {
---
+++ altlas/Assets/scripts/MoveStack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStack {
+
+    private static string rightControllerName = "Controller (right)"; //see resetStack() if you want another form of Input
+    private static string leftControllerName = "Controller (left)";
+    public static GameObject removedStack = null;
+    public static Vector3 removedStackPosition;
+    private static MapGenerator instance = new MapGenerator();
+    
+
+
+    public static bool objectIsFromAStack(GameObject obj)
+    {
+        if (obj == null) return false;
+        if (obj.transform.parent != null)
+        {
+            if (ControllerInteraction.heldObject.transform.parent.name.Contains(instance.EMPTY_ENDING))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void moveStackToDesk(GameObject stackToMove)
+    {
+        for (int i = 0; i < stackToMove.transform.childCount; i++)
+        {
+            instance.spreadGameObjectOnDesk(stackToMove.transform.GetChild(i).gameObject);
+        }
+        removedStack = stackToMove;
+    }
+
+    public static void resetStack()
+    {
+        string nameOfMapsObjects = removedStack.transform.name.Remove(removedStack.transform.name.Length - instance.EMPTY_ENDING.Length);
+        int i = 0;
+        foreach (GameObject map in GameObject.FindGameObjectsWithTag("Pickupable"))
+        {
+            if (map.name.Equals(nameOfMapsObjects))
+            {
+                if (map.transform.parent == null || map.transform.parent.Equals(rightControllerName) || map.transform.parent.Equals(leftControllerName))
+                {
+                    map.transform.parent = removedStack.transform;
+                }
+                map.transform.position = new Vector3(removedStackPosition.x, removedStackPosition.y + i * instance.MAPS_Y_OFFSET, removedStackPosition.z);
+                map.transform.localScale = new Vector3(0.1f, map.transform.localScale.y, 0.1f);
+                map.transform.localRotation = Quaternion.identity;
+            }
+            i++;
+        }
+        removedStack = null;
+    }
+}
