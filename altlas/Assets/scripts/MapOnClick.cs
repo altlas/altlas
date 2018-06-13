ac7883199dda4c7d338f16b7a427a73e80d8c256
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-06-13 15:33:33 +0200

    resized maps when being in the middle of the desk

--- altlas/Assets/scripts/MapOnClick.cs
+++ altlas/Assets/scripts/MapOnClick.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapOnClick : MonoBehaviour, ClickableInterface
 {
-    Vector3 MIDDLE_OF_DESK = new Vector3(-0.188f, 0.858f, 0.694f); //absolute positon
+    Vector3 MIDDLE_OF_DESK = new Vector3(-0.2f, 0.858f, 0.65f); //absolute positon
     public static GameObject MAP_ON_MIDDLE_OF_DESK = null; 
 
     // Use this for initialization
@@ -24,9 +24,11 @@ public class MapOnClick : MonoBehaviour, ClickableInterface
         if (MoveStack.removedStack != null) {
             if (MAP_ON_MIDDLE_OF_DESK != null) {
                 MAP_ON_MIDDLE_OF_DESK.transform.position = this.transform.position;
+                MAP_ON_MIDDLE_OF_DESK.transform.localScale = MoveStack.MAP_ON_FREE_AREA_OF_DESK_SCALE;
             }
             this.transform.position = MIDDLE_OF_DESK;
             this.transform.localRotation = Quaternion.identity;
+            this.transform.localScale = MoveStack.MAP_ON_MIDDLE_OF_DESK_SCALE;
             MAP_ON_MIDDLE_OF_DESK = this.gameObject;
         }
     }
--- altlas/Assets/scripts/MoveStack.cs
+++ altlas/Assets/scripts/MoveStack.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class MoveStack {
 
     private static string inputDeviceName = "Controller"; //see resetStack() if you want another form of Input
     public static GameObject removedStack = null;
     public static Vector3 removedStackPosition;
-    private static MapGenerator instance = new MapGenerator();
     public static string MAPTAG = "Pickupable";
-    
+
+    public static Vector3 DESK_FREE_AREA_LEFT_CORNER = new Vector3(-0.7105434f, 0.85f, 0.4124395f);
+    public static float DESK_FREE_AREA_LENGTH = 0.4f;
+    public static Vector3 MAP_IN_DRAWER_SCALE = ((GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Map_alt.prefab", typeof(GameObject))).transform.localScale;
+    public static Vector3 MAP_ON_FREE_AREA_OF_DESK_SCALE = MAP_IN_DRAWER_SCALE * 2;
+    public static Vector3 MAP_ON_MIDDLE_OF_DESK_SCALE = MAP_ON_FREE_AREA_OF_DESK_SCALE * 3;
+
+
 
 
     public static bool objectIsFromAStack(GameObject obj)
@@ -17,7 +24,7 @@ public class MoveStack {
         if (obj == null) return false;
         if (obj.transform.parent != null)
         {
-            if (obj.transform.parent.name.Contains(instance.EMPTY_ENDING))
+            if (obj.transform.parent.name.Contains(MapGenerator.EMPTY_ENDING))
             {
                 return true;
             }
@@ -30,7 +37,7 @@ public class MoveStack {
         int numberOfMapsInStack = stackToMove.transform.childCount;
         for (int i = 0; i < numberOfMapsInStack; i++)
         {
-            instance.spreadGameObjectOnDesk(stackToMove.transform.GetChild(0).gameObject);
+            spreadMapOnDesk(stackToMove.transform.GetChild(0).gameObject);
             stackToMove.transform.GetChild(0).gameObject.transform.parent = null;
         }
         removedStack = stackToMove;
@@ -38,7 +45,7 @@ public class MoveStack {
 
     public static void resetStack()
     {
-        string nameOfMapsObjects = removedStack.transform.name.Remove(removedStack.transform.name.Length - instance.EMPTY_ENDING.Length);
+        string nameOfMapsObjects = removedStack.transform.name.Remove(removedStack.transform.name.Length - MapGenerator.EMPTY_ENDING.Length);
         int i = 0;
         foreach (GameObject map in GameObject.FindGameObjectsWithTag(MAPTAG))
         {
@@ -48,12 +55,25 @@ public class MoveStack {
                 {
                     map.transform.parent = removedStack.transform;
                 }
-                map.transform.position = new Vector3(removedStackPosition.x, removedStackPosition.y + i * instance.MAPS_Y_OFFSET, removedStackPosition.z);
-                map.transform.localScale = new Vector3(0.1f, map.transform.localScale.y, 0.1f);
+                map.transform.position = new Vector3(removedStackPosition.x, removedStackPosition.y + i * MapGenerator.MAPS_Y_OFFSET, removedStackPosition.z);
+                map.transform.localScale = MAP_IN_DRAWER_SCALE;
                 map.transform.localRotation = Quaternion.identity;
                 i++;
             }
         }
         removedStack = null;
+    }
+
+    /**
+     * randomly spreads given map on desk
+     * map: game object to be spread
+     */
+    public static void spreadMapOnDesk(GameObject map)
+    {
+        float newX = DESK_FREE_AREA_LEFT_CORNER.x - Random.Range(0, DESK_FREE_AREA_LENGTH);
+        float newZ = DESK_FREE_AREA_LEFT_CORNER.z + Random.Range(0, DESK_FREE_AREA_LENGTH);
+        float newY = DESK_FREE_AREA_LEFT_CORNER.y + +Random.Range(0, 0.01f);
+        map.transform.position = (new Vector3(newX, newY, newZ));
+        map.transform.localScale = MAP_ON_FREE_AREA_OF_DESK_SCALE;
     }
 }
