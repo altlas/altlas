ba16106cf6a594d7b6180ed55ceed2e925d16de7
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-06-14 22:49:37 +0200

    refs #51: we got a lens now, which is pretty neat

---
+++ altlas/Assets/scripts/ZoomCameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomCameraFollow : MonoBehaviour
+{
+    public Transform MainCameramera;
+    private Transform _lens;
+
+    private void Start()
+    {
+        _lens = transform.parent;
+    }
+
+    private void Update()
+    {
+        // get vector from camera to lens and base lens direction
+        var mainCamToLens = _lens.position - MainCameramera.position;
+        var lensBaseDirection = _lens.forward;
+        // fix x rotation of cylinder
+        lensBaseDirection = Quaternion.AngleAxis(90, _lens.right) * lensBaseDirection;
+        // mix base dir and view dir
+        var mixedDir = lensBaseDirection * 0.4f + mainCamToLens * 0.6f;
+        transform.rotation = Quaternion.LookRotation(mixedDir);
+    }
+}
