280a19c871b47e642c7b1418681ab866657f071c
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-06-15 21:02:00 +0200

    astronomy maps are being moved to ceiling when lever ist clicked

---
+++ altlas/Assets/scripts/AstronomyMovingScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronomyMovingScript : MonoBehaviour {
+    public float speed = 3f;
+    public bool moving = false;
+    public bool expanded = false;
+    private Vector3 ceiling = new Vector3(0, 4.57f, 0);
+    private Vector3 ceilingSize = new Vector3(6.95f, 0.006f, 6.638f);
+    private float expandRate = 1.015f;
+    void Update()
+    {
+        if (moving)
+        {
+            var target = expanded ? ceiling: MoveStack.MIDDLE_OF_DESK;
+            if (transform.position == target)
+            {
+                moving = false;
+                transform.localScale = ceilingSize;
+                return;
+            }
+            var step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target, step);
+            transform.localScale *= expandRate;
+        }
+    }
+}
--- altlas/Assets/scripts/LeverScript.cs
+++ altlas/Assets/scripts/LeverScript.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LeverScript : MonoBehaviour, ClickableInterface {
+
   public GlobeMovingScript globeMovingScript;
-  
-  void ClickableInterface.onClick(){
-    globeMovingScript.moving = true;
-    globeMovingScript.expanded = !globeMovingScript.expanded;
+
+    void ClickableInterface.onClick(){
+        GameObject mapInMiddle = MoveStack.MAP_ON_MIDDLE_OF_DESK;
+        if (mapInMiddle != null) {
+            if (mapInMiddle.name.Equals("astronomie"))
+            {
+                AstronomyMovingScript astronomieScript = mapInMiddle.GetComponent<AstronomyMovingScript>();
+                astronomieScript.moving = true;
+                astronomieScript.expanded = !astronomieScript.expanded;
+                return;
+            }
+        }
+        globeMovingScript.moving = true;
+        globeMovingScript.expanded = !globeMovingScript.expanded;
+    
   }
 }
