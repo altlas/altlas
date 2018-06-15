1afdeee688d809c7c899f29ce96843d067778417
Joana Be <33000454+JoeAtHPI@users.noreply.github.com>
2018-06-15 02:38:21 +0200

    Revert "threw away useless code in ControllerInteraction"
    This reverts commit 7f59db97286953de5bdf9aceb61042d2a1e9efd6.

--- altlas/Assets/scripts/ControllerInteraction.cs
+++ altlas/Assets/scripts/ControllerInteraction.cs
@@ -5,8 +5,19 @@ public class ControllerInteraction : MonoBehaviour
 {
     private SteamVR_TrackedObject trackedObj;
 
+    /*private GameObject removedStack = null;
+    private Vector3 removedStackPosition;
+    MapGenerator instance = new MapGenerator();*/
+
     public static GameObject heldObject;
+    public static bool isHolding;
+
+    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private Valve.VR.EVRButtonId leftPadButton = Valve.VR.EVRButtonId.k_EButton_DPad_Left;
+    private Valve.VR.EVRButtonId upPadButton = Valve.VR.EVRButtonId.k_EButton_DPad_Up;
+    private Valve.VR.EVRButtonId rightPadButton = Valve.VR.EVRButtonId.k_EButton_DPad_Right;
+    private Valve.VR.EVRButtonId downPadButton = Valve.VR.EVRButtonId.k_EButton_DPad_Down;
 
      SteamVR_Controller.Device controller {
         get {
@@ -27,9 +38,21 @@ public class ControllerInteraction : MonoBehaviour
         {
             return;
         }
+        
+        if (controller.GetPressUp(gripButton))
+        {
+            if (heldObject != null && isHolding)
+            {
+                heldObject.transform.parent = null;
+                isHolding = false;
+            }
+
+        }
         if (controller.GetPressDown(triggerButton)) {
             triggerButtonPressedAction();
         }
+
+
     }
 
     public void triggerButtonPressedAction() {
@@ -42,15 +65,22 @@ public class ControllerInteraction : MonoBehaviour
             }
             if (MoveStack.objectIsFromAStack(heldObject) && MoveStack.removedStack != null)
             {
+                isHolding = false;
                 heldObject = null;
             }
         }
     }
 
+    private void holdObject() {
+        heldObject.transform.parent = this.transform;
+        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+        isHolding = true;
+    }
+
     private void OnTriggerEnter(Collider collider) {
         if (collider.gameObject.GetComponent<HighlightScript>() != null)
             collider.gameObject.GetComponent<HighlightScript>().OnRayEnter();
-        if (collider.gameObject.GetComponent<ClickableInterface>() != null) {
+        if (!isHolding && collider.gameObject.GetComponent<ClickableInterface>() != null) {
             heldObject = collider.gameObject;
             var mapScript = heldObject.GetComponent<MapScript>();
             if (!MoveStack.objectIsFromAStack(heldObject) && mapScript != null)
@@ -60,6 +90,9 @@ public class ControllerInteraction : MonoBehaviour
 
     private void OnTriggerExit(Collider collider)
     {
+        if (!isHolding)
+            heldObject = null;
+
         if (MoveStack.MAP_ON_MIDDLE_OF_DESK == null) {
             GameObject.Find(MoveStack.textDisplayName).GetComponent<TextMesh>().text = "Select a map!";
         }
